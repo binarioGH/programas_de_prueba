5a96b3adc81ffe74566aa5f716cc02cff193c528
binarioGH <lacuentafalsadediegojonas@gmail.com>
2018-04-20 18:33:47 -0500

    mejora en el programa de cifrado, ahora se pueden añadir numeros

--- cifrado_prueba.py
+++ cifrado_prueba.py
@@ -2,8 +2,8 @@
 #NO USES ESTO COMO UN CIFRADO REAL, ES EXTREMADAMENTE DEBIL
 import subprocess
 #NO USES ESTO COMO UN CIFRADO REAL, ES EXTREMADAMENTE DEBIL
-letterlist = (" ","a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
-Cyphlist = (" ","1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r")
+letterlist = ("1", "2", "3", "4", "5", "6", "7", "8", "9", "0", " " ,"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
+Cyphlist = ("s","t","u","v","w","x","y","z","/","%"," ","1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r")
 
 
 def cyph(list1, list2, word):
