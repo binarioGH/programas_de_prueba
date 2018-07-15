fd96a644354f01f859e19fa964f94d180da3b0d9
binarioGH <lacuentafalsadediegojonas@gmail.com>
2018-07-15 09:33:08 -0500

    Me di cuenta de que no se puede dividir un numero despues de su mitad.

--- vida/934.py
+++ vida/934.py
@@ -3,6 +3,6 @@ from sys import argv
 #Estaba en un camion con el numero 934, empecé a dividirlo hasta ver con que numeros daba % == 0.
 if __name__ == '__main__':
 	mx = int(argv[1])
-	for num in range(2, mx):
+	for num in range(2, int(mx/ 2 + 1)):
 		if mx % num == 0:
 			print("Es divisible entre {}: {}".format(num, int(mx/num)))
