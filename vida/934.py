55d1e9920715e7a2fe8f89c19ba57b380530998f
binarioGH <lacuentafalsadediegojonas@gmail.com>
2018-07-14 23:02:33 -0500

    Un programa que se me ocurrió mientras estaba en un camión

---
+++ vida/934.py
@@ -0,0 +1,8 @@
+#-*-coding: utf-8-*-
+from sys import argv
+#Estaba en un camion con el numero 934, empecé a dividirlo hasta ver con que numeros daba % == 0.
+if __name__ == '__main__':
+	mx = int(argv[1])
+	for num in range(2, mx):
+		if mx % num == 0:
+			print("Es divisible entre {}: {}".format(num, int(mx/num)))
