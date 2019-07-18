b4e483e2c20af605c538a944cf90c1ba2e32642f
binarioGH <lacuentafalsadediegojonas@gmail.com>
2019-07-17 21:09:41 -0700

    bug arreglado

--- smtp/fastpoc.py
+++ smtp/fastpoc.py
@@ -8,16 +8,17 @@ def main():
 	mail = imaplib.IMAP4_SSL("imap.gmail.com")
 	mail.login(user, password)
 	subject = input("Input a subject: ")
-	mail.select("INBOX")
+	mail.select("[Gmail]/All Mail")
 	result, data = mail.uid("search", None, 'HEADER Subject "{}"'.format(subject))
 	print(data)
 	mailIDs = data[0].split()
 	for ID in mailIDs:
-		print(ID)
+		
 		result, m = mail.uid("fetch", ID,"(RFC822)")
 		m = m[0][1].decode("utf-8")
 		raw = email.message_from_string(m)
 		print("-"*80)
-		print("From:{}\nSubject:".format(raw["From"],raw["Subject"]))
+		print(ID)
+		print("From:{}\nSubject:{}".format(raw["From"],raw["Subject"]))
 if __name__ == '__main__':
 	main()
